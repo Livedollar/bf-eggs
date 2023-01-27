66030d2029f07d942796b7af8a8e8f5c5d860866
deadcoin <deadcoin77@gmail.com>
2023-01-26 19:18:38 -0500

    update, still need to implement the burn and replace `Web3Button`: erc721 to erc1155

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-
+import { Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
@@ -6,7 +6,19 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-        <Header/>
+      <Header />
+      <section className={styles.info}>
+        <h1>My Egg Project</h1>
+        <p>Welcome to the test burn function. This is also the first (and only) artwork and design of these Egg characters I ever made.
+        </p>
+        <br />
+        <Web3Button contractAddress="0xcc0DfA67F235a2C473E7aC0236247f660Bbfcdb3" action={(contract) => {contract.erc721.claim(1);}}
+          
+         colorMode="light" accentColor="#9700f5" 
+        >
+        <center>Claim an egg</center>
+            </Web3Button>
+        </section>
         <p>Home</p>
         </div>  
   );
--- pages/nfts.tsx
+++ pages/nfts.tsx
@@ -5,38 +5,41 @@ import Header from "../components/Header";
 import styles from "../styles/Nfts.module.css";
 
 
-const Nfts: NextPage = () => {
-   const { contract } = useContract("0xcc0DfA67F235a2C473E7aC0236247f660Bbfcdb3");
-  const { data: nfts, isLoading: isReadingNfts } = useNFTs(contract);
+function Nfts() {
+  const { contract } = useContract("0xcc0DfA67F235a2C473E7aC0236247f660Bbfcdb3");
+  const { data: nfts, isLoading, error } = useNFTs(contract);
+
 
   console.log(nfts);
 
   return (
-    <div> className={styles.container}
-        <Header/>
-      <section className={styles.info}></section>              
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-           nfts?.map((nft) => {
-       
-             return (
-               <div key={nft.metadata.id}>
-                 <ThirdwebNftMedia
-                   metadata={nft.metadata}
-                   height="500"
-                   style={{ borderRadius: "10px" }}
-                 />
-                 <p>{nft.metadata.name}</p>
-                   
-                 
-               </div>
-             ); 
-        
+    <div className={styles.container}>
+      <Header />
+      <section className="={styles.info}"></section>
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        nfts?.map((nft) => {
+
+          return (
+
+            <div key={nft.metadata.id}>
+              <ThirdwebNftMedia
+                metadata={nft.metadata}
+                height="200"
+                style={{ borderRadius: "10px" }} />
+              <p>{nft.metadata.name}</p>
+
+
+            </div>
+          );
+        }))}
+    </div>);
+} 
      
       
           
-  );
-}
+  
 
 export default Nfts;
